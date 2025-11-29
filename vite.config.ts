d29937053b92e06cd6e9640d185c14f703d45c24
stibe881 <stefan.gross@hotmail.ch>
2025-11-29 09:56:13 +0100

    fix: Correctly configure PostCSS plugins in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -18,8 +20,8 @@ export default defineConfig(({ mode }) => {
       css: {
         postcss: {
           plugins: [
-            'tailwindcss',
-            'autoprefixer',
+            tailwindcss,
+            autoprefixer,
           ],
         },
       },
